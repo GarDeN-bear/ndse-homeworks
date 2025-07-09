07e088d760af952ed9166be79aada465103674ad
Denis Garpinich <d.garpinich@wisetech.pro>
2025-07-09 07:06:02 +0300

    005-http Выполнены все задания

--- 005-http/config.js
+++ 005-http/config.js
@@ -1,2 +1,2 @@
 export const weatherStackApiKey = process.env.WEATHERSTACK_API_KEY;
-export const weatherStackUrl = "https://weatherstack.com/";
+export const weatherStackUrl = "http://api.weatherstack.com/current";
--- 005-http/index.js
+++ 005-http/index.js
@@ -1,3 +1,55 @@
 import * as config from "./config.js";
+import http from "http";
+import readline from "readline";
+import { isNumberObject } from "util/types";
 
-console.log(config);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function handleCity(input) {
+  const exit = parseInt(input);
+
+  if (!isNaN(exit)) {
+    rl.close();
+    return;
+  }
+
+  const url = `${config.weatherStackUrl}?access_key=${config.weatherStackApiKey}&query=${input}`;
+  http
+    .get(url, (res) => {
+      if (res.statusCode !== 200) {
+        console.error(`Error: ${res.statusCode}, ${res.statusMessage}`);
+        rl.question("\nInput city: ", handleCity);
+        return;
+      }
+      let data = "";
+      res.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      res.on("end", () => {
+        try {
+          const weatherData = JSON.parse(data);
+          console.log("\nLocation:");
+          console.log(weatherData.location);
+          console.log("\nCurrent weather:");
+          console.log(weatherData.current);
+        } catch (err) {
+          console.error("Error parsing weather data:", err.message);
+        }
+        rl.question("\nInput city: ", handleCity);
+      });
+    })
+    .on("error", (err) => {
+      console.error("Error http get:", err.message);
+      rl.question("\nInput city: ", handleCity);
+    });
+}
+
+rl.question("\nInput city: ", handleCity);
+
+rl.on("close", () => {
+  console.log("Weather forecast is finished)");
+});
