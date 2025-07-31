424627fce1f1f5694652bb6fdeaf68cccb86f395
Денис Гарпинич <d.garpinich@wisetech.pro>
2025-07-31 20:10:58 +0300

    008 Доделано обновление и удаление

--- 008-ejs/routes/books.js
+++ 008-ejs/routes/books.js
@@ -42,47 +42,53 @@ router.get("/:id", (req, res) => {
   if (bookIndex !== -1) {
     res.status(200).render("view", {book: books[bookIndex]});
   } else {
-    res.status(404).render("view", {error: "Book isn't found" });
+    res.status(404);
   }
 });
 
-router.get("/:id/download", (req, res) => {
+router.post("/:id", (req, res) => {
   const { id } = req.params;
-  const bookIndex = books.findIndex((el) => el.id === id);
 
-  if (bookIndex !== -1 && books[bookIndex].fileBook) {
-    const filePath = books[bookIndex].fileBook;
-    res.status(200).download(filePath, path.basename(filePath));
-  } else {
-    res.status(404).json({ error: "Book isn't found" });
-  }
+  books = books.filter((el) => el.id !== id);
+  res.status(201).redirect('/api/books/');
 });
 
-router.put("/:id", uploader.single("file-key"), (req, res) => {
-  const { id } = req.params;
-
-  const fileBook = req.file ? req.file.path : null;
-
-  const { title, description, authors, favorite, fileCover, fileName } =
-    req.body;
-
+router.get("/update/:id", (req, res) => {
+    const { id } = req.params;
   const bookIndex = books.findIndex((el) => el.id === id);
 
   if (bookIndex !== -1) {
-    books[bookIndex] = {
-      id: books[bookIndex].id,
-      title: title,
-      description: description,
-      authors: authors,
-      favorite: favorite === "true",
-      fileCover: fileCover,
-      fileName: fileName,
-      fileBook: fileBook,
-    };
-    res.status(200).json(books[bookIndex]);
+    res.status(200).render("update", {title: "Updating book", book: books[bookIndex]});
   } else {
-    res.status(404).json({ error: "Book isn't found" });
+    res.status(404);
   }
+});
+
+router.post("/update/:id", (req, res) => {
+  const { id } = req.params;
+
+  const { title, description, authors, favorite, fileCover, fileName } =
+    req.body;
+  
+  const bookIndex = books.findIndex((el) => el.id === id);
+
+  if (bookIndex !== -1) {
+    const fileBook = req.file ? req.file.path : null;
+    books[bookIndex] = {
+        id: books[bookIndex].id,
+        title: title,
+        description: description,
+        authors: authors,
+        favorite: favorite === "true",
+        fileCover: fileCover,
+        fileName: fileName,
+        fileBook: fileBook,
+      };
+      res.status(201).redirect('/api/books/');
+  } else {
+    res.status(404);
+  }
+  
 });
 
 router.delete("/:id", (req, res) => {
